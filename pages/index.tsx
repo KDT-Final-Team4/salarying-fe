07788602405891ebdf1b372ad28b3e7a47d3eea5
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-03-07 16:54:51 +0900

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,4 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import firebase from '@/firebase';
-import { v4 } from 'uuid';
 import Main from '@/components/Main';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   // const apiKey = process.env.NEXT_PUBLIC_API_KEY;
