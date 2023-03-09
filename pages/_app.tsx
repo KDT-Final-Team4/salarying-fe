a88b4b5bfe57f1149c77c067c1fcdb5fc60dc9f1
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-03-09 12:38:49 +0900

    config: set up react-query & react-query-devtools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,29 @@
 import SideNav from '@/components/SideNav';
 // import '@/styles/reset.css';
 import '@/styles/globals.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
 import styled from 'styled-components';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: Infinity,
+    },
+  },
+});
 
 // 앱의 레이아웃을 결정하는 컴포넌트입니다.
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Wrapper>
-      <SideNav />
-      <Component {...pageProps} />
-    </Wrapper>
+    <QueryClientProvider client={queryClient}>
+      <Wrapper>
+        <SideNav />
+        <Component {...pageProps} />
+      </Wrapper>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
