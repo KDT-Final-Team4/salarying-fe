90c0511a4bcae10b13ec8b7f1d9638467443da9e
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-03-10 11:43:23 +0900

    test commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Main from '@/components/Main';
 
 export default function Home() {
-  // const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+  console.log(apiKey);
   return <Main>내용</Main>;
 }
