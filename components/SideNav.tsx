2d19a135cbad859d5a31089073909dede916cc44
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-03-20 15:04:07 +0900

    Create pages to URLs"

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -5,8 +5,11 @@ export default function SideNav() {
   return (
     <Wrapper>
       <ul>
-        <li>side item1</li>
-        <li>side item2</li>
+        <li>약관 관리</li>
+        <li>기업 회원관리</li>
+        <li>공지사항</li>
+        <li>FAQ</li>
+        <li>1:1 문의</li>
       </ul>
     </Wrapper>
   );
@@ -17,4 +20,19 @@ const Wrapper = styled.section`
   height: 100vh;
   width: 200px;
   background-color: aliceblue;
+  border: 3px solid red;
+  ul {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    align-items: center;
+    margin-top: 50px;
+    li {
+      box-sizing: border-box;
+      width: 100%;
+      padding: 0 10px;
+      border-bottom: 1px solid var(--color-gray300);
+    }
+  }
 `;
