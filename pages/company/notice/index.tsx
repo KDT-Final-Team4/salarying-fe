54d76bfdf0a8189f9ff6e905387a3bd2f375f8a7
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-03-26 20:57:19 +0900

    feat: company/notification wireframe markup

--- pages/company/notice/index.tsx
+++ pages/company/notice/index.tsx
@@ -1,5 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const listData = [
+  { title: '알바직 모집' },
+  { title: '인턴 모집' },
+  { title: 'CEO 모집' },
+  { title: '총무 모집' },
+];
 
 export default function Notice() {
-  return <div>index</div>;
+  return (
+    <Wrapper>
+      <Head>
+        <Title>지원자 관리</Title>
+      </Head>
+
+      <PostList></PostList>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 0 100px;
+  /* gap: 100px; */
+  border: 1px solid green;
+`;
+
+const Head = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 20px;
+  margin: 50px 0;
+  padding: 30px 0;
+  border-bottom: 1px solid black;
+`;
+const Title = styled.h1``;
+const PostList = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
--- pages/company/notification.tsx
+++ pages/company/notification.tsx
@@ -1,40 +1,96 @@
 import Button_1 from '@/components/ui/Button_1';
-import React from 'react';
+import React, { useState } from 'react';
 import { AiOutlineMail } from 'react-icons/ai';
 import styled from 'styled-components';
+import { useEffect } from 'react';
 
 const mails = [
   {
     title: '메일1',
+    id: '01',
+    checked: false,
   },
   {
     title: '메일2',
+    id: '02',
+    checked: false,
   },
   {
-    title: '메일2',
+    title: '메일3',
+    id: '03',
+    checked: false,
   },
 ];
+const Mail = ({ title, id, handleCheckChild, checked }: any) => {
+  return (
+    <>
+      <div>
+        <input
+          type='checkbox'
+          onChange={(event) => handleCheckChild(event, id)}
+          checked={checked}
+        />
+      </div>
+      <div>{title}</div>
+      <div>
+        <Button_1 {...sendBtnProps} />
+      </div>
+    </>
+  );
+};
+
 const sendBtnProps = {
   Icon: AiOutlineMail,
   name: 'send',
   color: '#047857',
 };
+
 const Notification = () => {
+  const [checked, setChecked] = useState(false); // 전체 체크박스의 체크 여부
+  const [childChecked, setChildChecked] = useState(mails); // 자식 체크박스들의 체크 여부
+
+  // 전체 체크박스가 변경되었을 때 호출되는 함수
+  const handleCheckAll = (event) => {
+    const masterChecked = event.target.checked;
+    setChecked(masterChecked);
+    const newChild = childChecked.map((child) => {
+      child.checked = masterChecked;
+      return child;
+    });
+    setChildChecked(newChild);
+  };
+
+  // 자식 체크박스 중 하나가 변경되었을 때 호출되는 함수
+  const handleCheckChild = (event, id) => {
+    // const newChildChecked = [...childChecked];
+    // newChildChecked[index] = event.target.checked;
+    const newChildChecked = childChecked.map((mail) => {
+      if (mail.id === id) {
+        mail.checked = event.target.checked;
+      }
+      return mail;
+    });
+    setChildChecked(newChildChecked);
+    setChecked(newChildChecked.every((mail) => mail.checked === true));
+  };
+
   return (
     <Wrapper>
-      <H1>메일함</H1>
+      <H1 onClick={() => console.log(childChecked)}>메일함</H1>
       <Panel>
-        <input type='checkbox' />
+        <input type='checkbox' checked={checked} onChange={handleCheckAll} />
         <span>전체 발송</span>
       </Panel>
       <Contents>
-        <div>
-          <input type='checkbox' />
-        </div>
-        <div>메일</div>
-        <div>
-          <Button_1 {...sendBtnProps} />
-        </div>
+        {mails.map((mail, index) => (
+          <Mail
+            key={index}
+            title={mail.title}
+            handleCheckChild={handleCheckChild}
+            checked={mail.checked}
+            id={mail.id}
+          />
+        ))}
       </Contents>
     </Wrapper>
   );
@@ -68,9 +124,5 @@ const Contents = styled.div`
   }
 `;
 
-const Mail = styled.div`
-  display: flex;
-  align-items: center;
-`;
 const MailTitle = styled.h3``;
 const MailBtns = styled.div``;
