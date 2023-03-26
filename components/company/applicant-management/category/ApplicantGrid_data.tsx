586a934c736dcf80df998949af7643fa243d0339
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-03-26 18:36:24 +0900

    add component: ui/Button_1.tsx"

--- components/company/applicant-management/category/ApplicantGrid_data.tsx
+++ components/company/applicant-management/category/ApplicantGrid_data.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import { AiOutlineCheckCircle, AiOutlineCloseSquare } from 'react-icons/ai';
 import styled from 'styled-components';
+import Button_1 from '../../../ui/Button_1';
+
+const rejectBtnProps = {
+  Icon: AiOutlineCloseSquare,
+  name: 'Reject',
+  color: '#47a89f',
+};
+
+const nextBtnProps = {
+  Icon: AiOutlineCheckCircle,
+  name: 'Next stage',
+  color: '#c25a5b',
+};
 
 function ApplicantGrid_data({ name }) {
   return (
     <>
       <Name>{name}</Name>
       <Btns>
-        <button>
-          <AiOutlineCloseSquare color='#c25a5b' />
-          Reject
-        </button>
-        <button>
-          <AiOutlineCheckCircle color='#47a89f' />
-          Next stage
-        </button>
+        <Button_1 {...rejectBtnProps} />
+        <Button_1 {...nextBtnProps} />
       </Btns>
     </>
   );
@@ -31,14 +38,4 @@ const Name = styled(Cell)``;
 const Btns = styled(Cell)`
   display: flex;
   gap: 5px;
-
-  button {
-    background-color: transparent;
-    border: 1px solid var(--color-gray300);
-    color: var(--color-gray600);
-    border-radius: 5px;
-    display: flex;
-    align-items: center;
-    gap: 5px;
-  }
 `;
--- pages/company/notification.tsx
+++ pages/company/notification.tsx
@@ -1,7 +1,76 @@
+import Button_1 from '@/components/ui/Button_1';
 import React from 'react';
+import { AiOutlineMail } from 'react-icons/ai';
+import styled from 'styled-components';
 
+const mails = [
+  {
+    title: '메일1',
+  },
+  {
+    title: '메일2',
+  },
+  {
+    title: '메일2',
+  },
+];
+const sendBtnProps = {
+  Icon: AiOutlineMail,
+  name: 'send',
+  color: '#047857',
+};
 const Notification = () => {
-  return <div>Notification</div>;
+  return (
+    <Wrapper>
+      <H1>메일함</H1>
+      <Panel>
+        <input type='checkbox' />
+        <span>전체 발송</span>
+      </Panel>
+      <Contents>
+        <div>
+          <input type='checkbox' />
+        </div>
+        <div>메일</div>
+        <div>
+          <Button_1 {...sendBtnProps} />
+        </div>
+      </Contents>
+    </Wrapper>
+  );
 };
 
 export default Notification;
+
+const Wrapper = styled.section`
+  padding: 20px;
+  width: 100%;
+`;
+
+const H1 = styled.h1`
+  font-weight: 700;
+  font-size: 20px;
+`;
+const Panel = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const Contents = styled.div`
+  flex-direction: column;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 50px 1fr 200px;
+  gap: 3px;
+  div {
+    display: flex;
+    align-items: center;
+    background-color: var(--color-emerald100);
+  }
+`;
+
+const Mail = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const MailTitle = styled.h3``;
+const MailBtns = styled.div``;
