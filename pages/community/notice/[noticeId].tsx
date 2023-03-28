653b43b401b79c58da06021ce4245c57e01818e6
hayden365 <ysmh100@gmail.com>
2023-03-28 20:05:37 +0900

    Feat: notice toggle

--- pages/community/notice/[noticeId].tsx
+++ pages/community/notice/[noticeId].tsx
@@ -1,67 +1,64 @@
-import React, { useState, useEffect } from "react";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-import styled from "styled-components";
+import React, { useState, useEffect } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import styled from 'styled-components'
 
 interface content {
-	noticeId: string;
-	title: string;
-	adminId: string;
-	date: string;
-	edit_date: string;
-	content: string;
-	state: boolean;
+  noticeId: string
+  title: string
+  adminId: string
+  date: string
+  edit_date: string
+  content: string
+  state: boolean
 }
 
 const getNotice = async (noticeId: string | string[]) => {
-	const result = await axios
-		.request({
-			method: "get",
-			url: `/api/notice/${noticeId}`,
-		})
-		.then(response => {
-			console.log(response.data.data.noticeId);
-			return response.data.data;
-		})
-		.catch(error => {
-			console.log(error);
-		});
-	return result;
-};
+  const result = await axios
+    .request({
+      method: 'get',
+      url: `/api/notice/${noticeId}`,
+    })
+    .then((response) => {
+      console.log(response.data.data.noticeId)
+      return response.data.data
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  return result
+}
 
 export default function NoticeDetail() {
-	const router = useRouter();
-	const noticeId = router.isReady ? router.query.noticeId : null;
-	console.log(noticeId);
+  const router = useRouter()
+  const noticeId = router.isReady ? router.query.noticeId : null
+  console.log(noticeId)
 
-	const { data } = useQuery(["notice", noticeId], () => getNotice(noticeId));
+  const { data } = useQuery(['notice', noticeId], () => getNotice(noticeId))
 
-	return (
-		<Wrapper>
-			<h2>{data?.title}</h2>
-			<span>{data?.content}</span>
-			<div>
-				<Link
-					href="/community/notice/edit/[noticeId]"
-					as={`/community/notice/edit/${noticeId}`}
-				>
-					수정
-				</Link>
-				<button>삭제</button>
-			</div>
-		</Wrapper>
-	);
+  return (
+    <Wrapper>
+      <h2>{data?.title}</h2>
+      <span>{data?.content}</span>
+      <div>
+        <Link href="/community/notice/edit/[noticeId]" as={`/community/notice/edit/${noticeId}`}>
+          수정
+        </Link>
+        <button>삭제</button>
+      </div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.div`
-	width: 100%;
-	margin: 10% auto;
-	padding: 0 50px;
-	box-sizing: border-box;
-	h2 {
-		font-size: 20px;
-		margin-bottom: 20px;
-	}
-`;
+  width: 100%;
+  margin: 10% auto;
+  padding: 0 50px;
+  box-sizing: border-box;
+  h2 {
+    font-size: 20px;
+    margin-bottom: 20px;
+  }
+`
--- pages/community/notice/index.tsx
+++ pages/community/notice/index.tsx
@@ -1,42 +1,44 @@
-import Link from "next/link";
-import axios from "axios";
-import { useQuery } from "@tanstack/react-query";
-import Content from "@/components/ui/Content";
-import styled from "styled-components";
-import { useMemo } from "react";
-import Table from "@/components/ui/Table";
+import Link from 'next/link'
+import { useState } from 'react'
+import axios from 'axios'
+import { useQuery } from '@tanstack/react-query'
+import Content from '@/components/ui/Content'
+import styled from 'styled-components'
 
 interface Object {
-  id: number;
-  title: string;
-  edit_id: string;
-  date: string;
-  state: boolean;
+  id: number
+  title: string
+  edit_id: string
+  date: string
+  state: boolean
+}
+interface StyledProps {
+  toggle: boolean
 }
 
 const getNotices = async () => {
   const result = await axios
     .request({
-      method: "get",
-      url: "/api/notice",
+      method: 'get',
+      url: '/api/notice',
     })
     .then((response) => {
-      return response.data.data;
+      return response.data.data
     })
     .catch((error) => {
-      console.log(error);
-    });
-  return result;
-};
+      console.log(error)
+    })
+  return result
+}
 
 export default function NoticeList() {
-  const { data: notices, isLoading } = useQuery(["notices"], getNotices);
+  const { data: notices, isLoading } = useQuery(['notices'], getNotices)
 
-  const headerArray = ["제목", "작성자", "작성날짜", "게시중"];
+  const headerArray = ['제목', '작성자', '작성날짜', '게시중']
 
   return (
     <Content title="공지사항">
-      <Link href={"/community/notice/new"}>등록</Link>
+      <Link href={'/community/notice/new'}>등록</Link>
       <List>
         <Top>
           {headerArray.map((header, idx) => (
@@ -47,41 +49,40 @@ export default function NoticeList() {
         </Top>
         {!isLoading &&
           notices?.map((notice, idx) => (
-            <Link
-              href="/community/notice/[noticeId]"
-              as={`/community/notice/${notice.id}`}
-              key={idx}
-            >
-              <ContentList>
-                <span className="item">{notice.title}</span>
-                <strong className="item">{notice.edit_id}</strong>
-                <time className="item">{notice.date}</time>
-              </ContentList>
-            </Link>
+            <ContentList key={idx}>
+              <Link href="/community/notice/[noticeId]" as={`/community/notice/${notice.id}`}>
+                <strong className="item">{notice.title}</strong>
+              </Link>
+              <span className="item">{notice.edit_id}</span>
+              <time className="item">{notice.date}</time>
+              <ToggleBtn toggle={notice.state}>
+                <Circle toggle={notice.state} />
+              </ToggleBtn>
+            </ContentList>
           ))}
       </List>
     </Content>
-  );
+  )
 }
 
 const List = styled.div`
   color: var(--color-gray600);
-`;
+`
 const Top = styled.section`
   width: 100%;
   height: 80px;
   align-items: center;
   justify-content: center;
-  font-size: 20px;
+  font-size: 16px;
+  color: var(--color-gray400);
+  font-weight: 700;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   margin: 50px 0;
-  border-radius: 40px;
-  background-color: var(--color-point);
   .item {
     margin: 0 auto;
   }
-`;
+`
 
 const ContentList = styled.section`
   width: 100%;
@@ -91,9 +92,43 @@ const ContentList = styled.section`
   grid-template-columns: 1fr 1fr 1fr 1fr;
   height: 80px;
   border-radius: 40px;
-  box-shadow: 0 1px 1px var(--color-point), 0 8px 8px var(--color-point);
+  border-bottom: 2px solid var(--color-lightgray);
   margin: 0 0 30px 0;
   .item {
     margin: 0 auto;
   }
-`;
+  > strong {
+    font-weight: 700;
+    color: var(--color-gray600);
+  }
+  > span {
+    font-weight: 700;
+    color: var(--color-gray500);
+  }
+  > time {
+    font-weight: 700;
+    color: var(--color-gray500);
+  }
+`
+const ToggleBtn = styled.button<StyledProps>`
+  width: 130px;
+  height: 50px;
+  border-radius: 30px;
+  border: none;
+  cursor: pointer;
+  background-color: ${(props) => (!props.toggle ? 'none' : 'var()')};
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.5s ease-in-out;
+`
+const Circle = styled.div<StyledProps>`
+  background-color: white;
+  width: 38px;
+  height: 38px;
+  border-radius: 50px;
+  position: absolute;
+  left: 5%;
+  transition: all 0.5s ease-in-out;
+`
