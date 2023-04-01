5bc9dd0efe2fc587c1c30b19b8916e4da7923c87
hwisaac <54179672+hwisaac@users.noreply.github.com>
2023-04-01 23:09:49 +0900

    Fix: deploy error

--- pages/api/notice/index.ts
+++ pages/api/notice/index.ts
@@ -1,35 +1,33 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 enum httpMethod {
-  POST = "POST",
-  GET = "GET",
-  PUT = "PUT",
-  PATCH = "PATCH",
-  DELETE = "DELETE",
+  POST = 'POST',
+  GET = 'GET',
+  PUT = 'PUT',
+  PATCH = 'PATCH',
+  DELETE = 'DELETE',
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   switch (req.method) {
     case httpMethod.GET:
       return res.status(200).json({
         stateCode: 200,
         success: true,
+        message: 'ok',
         data: [
           {
             id: 1,
-            title: "공지사항 제목 1",
-            edit_id: "aaa@aaa.com",
-            date: "YYYY-MM-DD HH-MM-SS",
+            title: '공지사항 제목 1',
+            edit_id: 'aaa@aaa.com',
+            date: 'YYYY-MM-DD HH-MM-SS',
             state: true,
           },
           {
             id: 2,
-            title: "공지사항 제목 2",
-            edit_id: "aaa@aaa.com",
-            date: "YYYY-MM-DD HH-MM-SS",
+            title: '공지사항 제목 2',
+            edit_id: 'aaa@aaa.com',
+            date: 'YYYY-MM-DD HH-MM-SS',
             state: false,
           },
         ],
